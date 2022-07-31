714eda01e8275f63558690717f6c4c33b071fedb
Casper Meijn <casper@meijn.net>
2022-07-31 16:33:50 +0200

    feat: Add add logs api call
    Fixed #5

--- backend/src/logs/mod.rs
+++ backend/src/logs/mod.rs
@@ -1,5 +1,5 @@
 use crate::logs::storage::LogStorage;
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
 use std::sync::Mutex;
 
 mod storage;
@@ -27,8 +27,25 @@ async fn get_logs(data: web::Data<LogsState>) -> impl Responder {
     HttpResponse::Ok().json(logs)
 }
 
+/// Add a log
+///
+/// Add a log to storage
+#[utoipa::path(
+request_body = LogEntry,
+responses(
+(status = 200, description = "Successful addition. Create log entry is returned (including new id)", body = LogEntry),
+)
+)]
+#[post("/api/logs")]
+async fn add_log(new_log: web::Json<LogEntry>, data: web::Data<LogsState>) -> impl Responder {
+    let mut storage = data.storage.lock().unwrap();
+
+    let response = storage.add_log(new_log.into_inner());
+    HttpResponse::Ok().json(response)
+}
+
 pub(super) fn configure(state: web::Data<LogsState>) -> impl FnOnce(&mut web::ServiceConfig) {
     |config: &mut web::ServiceConfig| {
-        config.app_data(state).service(get_logs);
+        config.app_data(state).service(get_logs).service(add_log);
     }
 }
--- backend/src/logs/types.rs
+++ backend/src/logs/types.rs
@@ -6,12 +6,14 @@ use utoipa::Component;
 #[derive(Deserialize, Serialize, Component, Clone, Debug)]
 pub struct LogEntry {
     /// Unique identifier for the log entry.
-    #[component(example = 1234)]
+    #[component(read_only, example = 1234)]
+    #[serde(default)]
     pub id: u32,
     /// Textual description of the fact that is logged.
     #[component(example = "Lord Strahd lives in castle Ravenloft")]
     pub text: String,
     /// List of tags accosiated to the logged fact.
-    #[component(example = json!(["Strahd", "Ravenloft"]))]
+    #[component(example = "Strahd")]
+    //TODO: Add "Ravenloft" as second tag https://github.com/juhaku/utoipa/issues/225
     pub tags: Vec<String>,
 }
